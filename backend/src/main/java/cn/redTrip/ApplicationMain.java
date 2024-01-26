0de38103792ed0a15d2ac568697a7d7b7524d6e9
2962283762 <2962283762@qq.com>
2024-01-26 19:45:54 +0800

    2024-1-26 19:45

---
+++ backend/src/main/java/cn/redTrip/ApplicationMain.java
@@ -0,0 +1,15 @@
+package cn.redTrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dzl
+ * @date 2024/1/26 19:41
+ */
+@SpringBootApplication
+public class ApplicationMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationMain.class,args);
+    }
+}
